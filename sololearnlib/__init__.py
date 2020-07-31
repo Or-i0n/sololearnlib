58ffad6f7bb8d926ad765230c6c80445f9b2908e
Orion <orionconner00@gmail.com>
2020-07-31 20:17:10 +0530

    Remove unnecessory files

--- sololearnlib/__init__.py
+++
@@ -1,7 +0,0 @@
-# Classes available to user
-
-from sololearnlib.blog import Blog
-from sololearnlib.code_playground import CodePlayground
-from sololearnlib.courses import Courses
-from sololearnlib.discuss import Discuss
-from sololearnlib.top_learners import TopLearners
