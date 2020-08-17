2351c9ddc3ec3a4cee98603fffaf224d3ae06664
Orion <orionconner00@gmail.com>
2020-08-17 18:26:20 +0530

    Change in version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as infile:
 
 setuptools.setup(
     name="sololearnlib",
-    version="2.0.1",
+    version="3.0.0",
     author="OR!ON",
     author_email="orionconner00@gmail.com", 
     license="MIT",
