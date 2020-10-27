c4aa8b0ad4b033e3cd7be8dee96af042518f54e7
orion <orionconner00@gmail.com>
2020-10-27 08:58:07 +0530

    Version changed to 3.0.0

--- sololearnlib/__init__.py
+++ sololearnlib/__init__.py
@@ -2,6 +2,6 @@
 __author__ = "OR!ON"
 __copyright__ = "Copyright 2020"
 __license__ = "MIT"
-__version__ = "2.0.1"
+__version__ = "3.0.0"
 __maintainer__ = "OR!ON"
-__status__ = "Beta"
+__status__ = "Stable"
