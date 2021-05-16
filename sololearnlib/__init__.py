e67bea3a49fb4fb65709b43965dfccaf8cd10f5d
Or-i0n <orionconner00@gmail.com>
2021-05-16 12:44:35 +0530

    Minor Change

--- sololearnlib/__init__.py
+++ sololearnlib/__init__.py
@@ -1,7 +1,8 @@
-# Import classes from files to directly access them when initializing sololearnlib
+# Import classes from files to directly access them when initializing 
+# sololearnlib.
 from sololearnlib.blog import Blog
-from sololearnlib.courses import Courses
 from sololearnlib.code_playground import CodePlayground
+from sololearnlib.courses import Courses
 from sololearnlib.discuss import Discuss
 from sololearnlib.top_learners import TopLearners
 
