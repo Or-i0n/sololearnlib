cab0e6f7b1dc2907aa6fb5f79ce68a32ffdc1a93
Or-i0n <orionconner00@gmail.com>
2021-05-17 18:32:24 +0530

    Minor patch

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf8") as infile:
 
 setuptools.setup(
     name="sololearnlib",
-    version="4.0.0",
+    version="4.0.1",
     author="OR!ON",
     author_email="orionconner00@gmail.com", 
     license="MIT",
--- sololearnlib/__init__.py
+++ sololearnlib/__init__.py
@@ -4,7 +4,7 @@
 from datetime import date
 
 __name__="sololearnlib"
-__version__ = "4.0.0"
+__version__ = "4.0.1"
 __description__="Library that retrieves public data from sololearn.com"
 __author__ = "OR!ON"
 __copyright__ = f"Copyright 2020-{date.today().year}"
