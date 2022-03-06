a34743fdc8c3ae28ff3cb06160567847588a37a9
Or-i0n <orionconner00@gmail.com>
2022-03-06 19:27:38 +0530

    Update imports

--- sololearnlib/__init__.py
+++ sololearnlib/__init__.py
@@ -14,6 +14,5 @@ __status__ = "Beta"
 
 from sololearnlib.blog import Blog
 from sololearnlib.code_playground import CodePlayground
-from sololearnlib.courses import Courses
 from sololearnlib.discuss import Discuss
 from sololearnlib.top_learners import TopLearners
